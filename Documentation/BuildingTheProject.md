2cb3e339de3cd43b01cd2006710e2237fe681de7
jam54 <git@jam54.com>
2023-11-22 14:56:36 +0100

    Added the AppName title, a description explaining to the user what they should do, a QR code + a button that shows the APK file in file explorer

--- Documentation/BuildingTheProject.md
+++ Documentation/BuildingTheProject.md
@@ -8,11 +8,11 @@
 
 
 - Edit `package.json`, `tauri.conf.json` and `Cargo.toml` to update the version number
-- Replace all instances of `"AppName"` inside `tauri.conf.json` with the name of the Android app we are building the project for
+- Replace all instances of `"AppName"` inside `tauri.conf.json` and `main.ts` with the name of the Android app we are building the project for
 - Run `npm i`
 - Inside the `src-tauri/resources` folder, replace the following placeholder files
-  - `QR.svg`: The image file for the QR should have `QR` as the filename, the extension doesn't matter, as long as it can be rendered by the OS' webview.
-  - `AppName.apk`: When it comes to the APK file, the filename doesn't matter, but the extension of the file has to be `.apk`.
+  - `QR.svg`: The image file for the QR should have `QR.svg` as the filename.
+  - `AppName.apk`: When it comes to the APK file the filename should match the string we used to replace "AppName" inside `tauri.conf.json` and `main.ts`, the extension of the file has to be `.apk`.
 - Replace the `app-icon.png` file in the root directory with the Android app icon we are building the project for, make sure the image file meets the following requirements:
   - A PNG file with transparency
   - 1024x1024 px
--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,102 @@
+<script lang="ts">
+    import {resolveResource, resourceDir} from "@tauri-apps/api/path";
+    import {convertFileSrc} from "@tauri-apps/api/tauri";
+    import {tauri} from "@tauri-apps/api";
+
+    export let appName: string;
+
+    async function showAPKInFolder()
+    {
+        const path = (await resourceDir()).substring(4) + "resources\\" +  appName + ".apk";
+
+        await tauri.invoke('show_in_folder', {path});
+    }
+</script>
+
 <main class="container">
-  <h1>Hello World!</h1>
+    <h1>{appName}</h1>
+    <div class="qrTextHolder">
+        <div class="textHolder">
+            <p>
+                Scan the QR code using your phone to download the APK directly, or alternatively, utilize the button below to locate and manually transfer the APK from your computer to your phone.
+            </p>
+        </div>
+        {#await resolveResource("resources/QR.svg") then qrImage}
+            <img class="qr" src={convertFileSrc(qrImage)} alt="">
+        {/await}
+    </div>
+    <button on:click={showAPKInFolder}>Show in folder</button>
 </main>
+
+<style>
+    .container {
+        display: flex;
+        flex-direction: column;
+        height: calc(100vh - 1.5em);
+        justify-content: space-between;
+    }
+
+    .qrTextHolder {
+        display: flex;
+        overflow: hidden;
+        flex-grow: 2;
+        padding: 0 1.5em;
+        justify-content: space-between;
+        height: 100%;
+    }
+
+    .textHolder {
+        display: flex;
+        flex-flow: column;
+        justify-content: space-between;
+        width: 100%;
+    }
+
+    h1 {
+        text-align: center;
+        font-weight: bold;
+    }
+
+    p {
+        font-size: large;
+        margin: 0;
+        padding-right: 1em;
+    }
+
+    .qr {
+        height: min(15em, 45vh);
+        align-self: flex-start;
+    }
+
+    button {
+        border: none;
+        /*width: 100%;*/
+        width: calc(100vw - 2.5em);
+        margin: 0 1.25em;
+        padding: 1.75em 0;
+        text-align: center;
+        text-decoration: none;
+        font-size: larger;
+        transition-duration: 0.4s;
+        cursor: pointer;
+        background-color: rgba(114, 137, 218, 0.1);
+        border-radius: 5px;
+        color: #98aae7;
+    }
+
+    button:hover {
+        background-color: rgba(114, 137, 218, 0.3);
+        color: white;
+    }
+
+    * {
+        font-family: "Nunito", sans-serif;
+        font-weight: normal;
+        color: white;
+    }
+
+    @font-face {
+        font-family: 'Nunito';
+        src: url('./fonts/Nunito/Nunito-VariableFont_wght.ttf');
+    }
+</style>
